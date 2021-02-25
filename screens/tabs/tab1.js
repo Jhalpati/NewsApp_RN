08a1a7578a853251dbcef5a4497cd8fdf831bf26
Jaykumar Halpati <rameshhalpati007@gmail.com>
2021-02-25 19:12:16 +0000

    Added a demo list

--- screens/tabs/tab1.js
+++ screens/tabs/tab1.js
@@ -1,15 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Container, Header, Content, List, ListItem, Thumbnail, Text, Left, Body, Right, Button } from 'native-base';
+export default class Tab1 extends Component {
+  render() {
+    return (
+      <Container>
+        <Header />
+        <Content>
+          <List>
+            <ListItem thumbnail>
+              <Left>
+                <Thumbnail square source={{ uri: 'https://picsum.photos/200' }} />
+              </Left>
+              <Body>
+                <Text>Joy</Text>
+                <Text note numberOfLines={1}>Its time to build a difference . .</Text>
+              </Body>
+              <Right>
+                <Button transparent>
+                  <Text>View</Text>
+                </Button>
+              </Right>
+            </ListItem>
 
-export default class Tab1 extends Component{
-    render(){
-        return(
-            <View style={StyleSheet.container}>
-                <Text>
-                        Tab1
-                </Text>
-            </View>
-        );
-    }
+            <ListItem thumbnail>
+              <Left>
+                <Thumbnail square source={{ uri: 'https://picsum.photos/200' }} />
+              </Left>
+              <Body>
+                <Text>Roy</Text>
+                <Text note numberOfLines={1}>Its time to build a train . .</Text>
+              </Body>
+              <Right>
+                <Button transparent>
+                  <Text>View</Text>
+                </Button>
+              </Right>
+            </ListItem>
+          </List>
+        </Content>
+      </Container>
+    );
+  }
 }
