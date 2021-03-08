a73ee93719aaa2d4a00d686c8a7e9470c1a4316d
Jaykumar Halpati <rameshhalpati007@gmail.com>
2021-03-08 12:49:10 +0000

    Added Header

--- screens/TabScreen.js
+++ screens/TabScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Container, Header, Content, Tab, Tabs } from 'native-base';
+import { Container, Header, Content, Tab, Tabs, Left, Right, Body, Title } from 'native-base';
 import Tab1 from './tabs/tab1';
 import Tab2 from './tabs/tab2';
 import Tab3 from './tabs/tab3';
@@ -8,11 +8,13 @@ export default class TabsExample extends Component {
   render() {
     return (
       <Container>
-        {/* <Header style={{backgroundColor:'black'}} hasTabs>
-          <body>
-          <title style={{color:'#ffffff'}}>News App</title>
-          </body>
-          </Header>  */}
+        <Header style={{backgroundColor:'lightblue'}} hasTabs>
+          <Left/>
+          <Body>
+          <Title>News App</Title>
+          </Body>
+          <Right/>
+          </Header> 
         <Tabs>
           <Tab heading="Tab1">
             <Tab1 />
@@ -24,8 +26,6 @@ export default class TabsExample extends Component {
             <Tab3 />
           </Tab>
         </Tabs>
-        
-
       </Container>
     );
   }
