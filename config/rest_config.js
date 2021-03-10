2997aee2eac2d4ecbc0a8378361f41e0b82b93cd
Jaykumar Halpati <rameshhalpati007@gmail.com>
2021-03-10 15:34:27 +0000

    Restructred API url

--- config/rest_config.js
+++ config/rest_config.js
@@ -1,1 +1,5 @@
-export const articles_url = 'http://newsapi.org/v2/top-headlines?country=gb&category=business&apiKey=6e257f34188c4b608d5558758b35bf2f';
+
+
+export const articles_url = 'http://newsapi.org/v2/top-headlines?';
+export const country_code = 'us';
+export const api_key = '6e257f34188c4b608d5558758b35bf2f';
